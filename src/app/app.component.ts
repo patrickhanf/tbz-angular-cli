cdba0f2caf8d6ab3a661cb08c295502a76fbd354
PHANF-PC\phanf <patrickhanf@gmail.com>
2017-03-01 16:52:34 -0600

    Added Material Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+// https://medium.com/front-end-hacking/getting-started-using-angular-material-2-in-an-angular-2-angular-cli-application-bbeecdf6bfe2#.fa6qj090d
+//import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 //comment
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //directives: [MD_BUTTON_DIRECTIVES],
+  template: '<button md-raised-button color=’primary’>this is a magical button</button>'
 })
 export class AppComponent {
   title = 'Trail Blazer go!';
