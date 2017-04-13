8878044c3d37a918375d3dab631237b67660887f
PHANF-PC\phanf <patrickhanf@gmail.com>
2017-04-13 09:03:10 -0500

    missed mock-contacts and address

---
+++ src/app/contacts/mock-contacts.ts
@@ -0,0 +1,17 @@
+//
+// https://angular.io/docs/ts/latest/tutorial/toh-pt4.html
+//
+import { ContactVM } from '../_models/contact';
+
+export const CONTACTS: ContactVM[] = [
+  { ContactId: 11, FirstName: 'Mr. Nice', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 12, FirstName: 'Narco', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 13, FirstName: 'Bombasto', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 14, FirstName: 'Celeritas', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 15, FirstName: 'Magneta', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 16, FirstName: 'RubberMan', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 17, FirstName: 'Dynama', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 18, FirstName: 'Dr IQ', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 19, FirstName: 'Magma', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] },
+  { ContactId: 20, FirstName: 'Tornado', LastName: "Guy", Address: [{ "AddressId": 0, "Street": "SSS" }] }
+];
